6a3756c1868a6322ecdec2ff4ce36599119d66ed
elvis <elvispeng@yunify.com>
2018-11-10 21:58:20 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# boilerplate
+
+
+> A Simple Boilerplate with Webpack 4, Eslint (Airbnb), Babel 7, React, SCSS.
+
+## Build Setup
+
+```bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run start
+```
---
+++ webpack.config.base.js
@@ -0,0 +1,139 @@
+const path = require('path');
+const webpack = require('webpack');
+const WebpackBar = require('webpackbar');
+const eslintFriendlyFormatter = require('eslint-friendly-formatter');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const notifier = require('node-notifier');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const { NODE_ENV } = process.env;
+const isDev = NODE_ENV !== 'production';
+
+const resolve = dir => path.join(__dirname, dir);
+
+const config = {
+  mode: process.env.NODE_ENV,
+  target: 'web',
+  entry: './src/index.js',
+  output: {
+    path: resolve('dist'),
+    publicPath: '/',
+    filename: 'js/[name].[hash].js',
+    chunkFilename: 'js/[name].[hash].js',
+    pathinfo: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: /src/,
+        exclude: /node_modules/,
+        options: {
+          formatter: eslintFriendlyFormatter,
+        },
+      },
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader',
+        include: /src/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        include: /src/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: { sourceMap: isDev },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              localIdentName: isDev ? '[path]-[name]_[local]' : '[name]_[local]_[hash:5]', // [hash:base64]
+              modules: true,
+              sourceMap: isDev,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: isDev },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              sourceMap: isDev,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    modules: [resolve('src'), resolve('node_modules')],
+    alias: {
+      src: resolve('src'),
+    },
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  stats: {
+    assetsSort: '!size',
+    children: false,
+    chunks: false,
+    colors: true,
+    entrypoints: false,
+    modules: false,
+  },
+  performance: {
+    hints: false,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': NODE_ENV,
+    }),
+    new WebpackBar({
+      name: NODE_ENV,
+      color: 'green',
+      profile: !isDev,
+    }),
+    new webpack.EnvironmentPlugin(['NODE_ENV']),
+    new FriendlyErrorsWebpackPlugin({
+      clearConsole: false,
+      onErrors: (severity, errors) => {
+        if (severity !== 'error') {
+          return;
+        }
+        const error = errors[0];
+        notifier.notify({
+          title: 'Webpack error',
+          message: `${severity}: ${error.name}`,
+          subtitle: error.file || '',
+        });
+      },
+    }),
+  ],
+};
+
+if (process.env.npm_config_report) {
+  config.plugins.push(new BundleAnalyzerPlugin({ analyzerPort: 3001 }));
+}
+
+module.exports = config;
